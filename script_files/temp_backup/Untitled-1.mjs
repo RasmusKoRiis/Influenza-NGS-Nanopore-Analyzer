73e4ac37cc018c643dc18c3e080b41886443ec39
RasmusKoRiis <rasmuskoriis@gmail.com>
2022-10-05 14:03:30 +0200

    Removed files

--- script_files/temp_backup/Untitled-1.mjs
+++
@@ -1,8 +0,0 @@
-import fetch from 'node-fetch';
-
-const url = "https://www.hvakosterstrommen.no/api/v1/prices/2022/10-04_NO5.json"
-
-const response = await fetch(url);
-const data = await response.json();
-
-console.log(data);
